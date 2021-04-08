bd3987e63e007b788882aca891ad93a5d70a09a2
Luis Lopez-Bonilla <me@luislopez.dev>
2021-04-08 18:32:47 -0400

    Updates

---
+++ precache-manifest.0ac7e37865cf2fddcf9c857930dc1e24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9253d6a33f0558103de4",
+    "url": "/static/css/main.9d4f5c8a.chunk.css"
+  },
+  {
+    "revision": "9253d6a33f0558103de4",
+    "url": "/static/js/main.2beef98a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bafbd00845ef46f82201",
+    "url": "/static/js/2.9d7921b3.chunk.js"
+  },
+  {
+    "revision": "29c7670070dec08896dc245cace66202",
+    "url": "/static/media/luislogo.29c76700.png"
+  },
+  {
+    "revision": "0442017de1cc08663232b7656cd6a712",
+    "url": "/static/media/luisprofile.0442017d.jpg"
+  },
+  {
+    "revision": "dfea2083ef59dc2ed21204b209f6eb9c",
+    "url": "/index.html"
+  }
+];
